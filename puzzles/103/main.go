08cc6c0e17ee59e7a4e846c49bb418ee005c3b1e
Philip Aldridge <philip@mohc.net>
2025-12-03 08:26:34 +0000

    day 3

--- puzzles/103/main.go
+++ puzzles/103/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/PhilAldridge/aoc-2024-go/pkg/files"
+	"github.com/PhilAldridge/aoc-2024-go/pkg/ints"
 )
 
 func main() {
@@ -13,9 +16,39 @@ func main() {
 }
 
 func part1(name string) int {
-	return 0
+	lines:= files.ReadLines(name)
+	total:=0
+	for _,line:= range lines {
+		total += findMaxVoltage(line, 2)
+	}
+	return total
 }
 
 func part2(name string) int {
-	return 0
+	lines:= files.ReadLines(name)
+	total:=0
+
+	for _, line:= range lines {
+		total += findMaxVoltage(line,12)
+	}
+
+	return total
 }
+
+func findMaxVoltage(input string, digitsRemaining int) (int) {
+	max:=-1
+	maxIndex:=0
+	for i:=0; i<=len(input)-digitsRemaining; i++ {
+		next:= ints.FromString(string(input[i]))
+		if next>max {
+			max = next
+			maxIndex = i
+		}
+	}
+
+	if digitsRemaining == 1 {
+		return max
+	}
+
+	return max*ints.Pow(10,digitsRemaining-1) + findMaxVoltage(input[maxIndex+1:],digitsRemaining-1)
+} 
