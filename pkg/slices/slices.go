ffdb746c4d2c8d8789e14928d06a9741534f812d
Philip Aldridge <philip@mohc.net>
2025-12-18 16:47:45 +0000

    create new util functions

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -1,10 +1,55 @@
 package slices
 
+import (
+	"strings"
+
+	"github.com/PhilAldridge/aoc-2024-go/pkg/ints"
+)
+
 func SlidingWindow(slice []int, size int) [][]int {
 	var result [][]int = make([][]int, 0)
 	for i := 0; i <= len(slice)-size; i++ {
 		window := slice[i : i+size]
 		result = append(result, window)
 	}
+	return result
+}
+
+func StringSliceToStringMapStringSlice(input []string, separator1 string, separator2 string) map[string][]string {
+	result := make(map[string][]string)
+
+	for _,line:= range input {
+		split1:= strings.Split(line,separator1)
+		split2:= strings.Split(split1[1],separator2)
+
+		result[split1[0]] = split2
+	}
+
+	return result
+}
+
+func StringSliceToStringMapIntSlice(input []string, separator1 string, separator2 string) map[string][]int {
+	result := make(map[string][]int)
+
+	for _,line:= range input {
+		split1:= strings.Split(line,separator1)
+		split2:= strings.Split(split1[1],separator2)
+
+		result[split1[0]] = ints.FromStringSlice(split2)
+	}
+
+	return result
+}
+
+func StringSliceToIntMapIntSlice(input []string, separator1 string, separator2 string) map[int][]int {
+	result := make(map[int][]int)
+
+	for _,line:= range input {
+		split1:= strings.Split(line,separator1)
+		split2:= strings.Split(split1[1],separator2)
+
+		result[ints.FromString(split1[0])] = ints.FromStringSlice(split2)
+	}
+
 	return result
 }
